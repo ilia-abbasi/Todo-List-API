daafab13d804730ae8a69100aa2591205a43d0ed
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-16 22:24:03 +0330

    Added main router

--- app/index.mjs
+++ app/index.mjs
@@ -1,6 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
+import mainRouter from "../routes/main.mjs";
+
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(mainRouter);
 
 app.listen(port, () => {
   console.log(`Server: Listening on port ${port} ...`);
