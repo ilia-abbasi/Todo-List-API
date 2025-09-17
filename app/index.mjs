cb55f5afc47eedf926e994376288938275deddf3
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-17 11:33:44 +0330

    Added db.mjs and used error handler
    - Installed morgan

--- app/index.mjs
+++ app/index.mjs
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
+import morgan from "morgan";
 import express from "express";
 import mainRouter from "../routes/main.mjs";
+import { generalErrorHandler } from "../helpers/response.mjs";
 
 dotenv.config();
 
@@ -8,7 +10,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(morgan(":method :url :status - :response-time ms"));
 app.use(mainRouter);
+
+app.use(generalErrorHandler);
 
 app.listen(port, () => {
   console.log(`Server: Listening on port ${port} ...`);
