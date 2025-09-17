d7bda3405590c099a4e50f0a4041ae42654052bd
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-17 14:04:59 +0330

    Added login route and stub controller

---
+++ controllers/login.mjs
@@ -0,0 +1,7 @@
+import _ from "lodash";
+import bcrypt from "bcryptjs";
+import { matchedData, validationResult } from "express-validator";
+
+async function loginUser(req, res, next) {
+  //
+}
--- routes/main.mjs
+++ routes/main.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 
 import registerRouter from "./register.mjs";
+import loginRouter from "./login.mjs";
 
 const router = express.Router();
 
 router.use("/register", registerRouter);
+router.use("/login", loginRouter);
 
 export default router;
