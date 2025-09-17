00cf7f48d28c05e73239b0a9e7a0b7a63fad76f4
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-17 14:13:36 +0330

    Added validation check inside login controller

--- controllers/login.mjs
+++ controllers/login.mjs
@@ -3,7 +3,18 @@ import bcrypt from "bcryptjs";
 import { matchedData, validationResult } from "express-validator";
 
 async function loginUser(req, res, next) {
-  //
+  const validationErrors = validationResult(req).errors;
+
+  if (!_.isEmpty(validationErrors)) {
+    const resObj = makeResponseObj(false, validationErrors[0].msg);
+    const statusCode = 400;
+
+    if (resObj.message === "Bad credentials") statusCode = 401;
+
+    return res.status(statusCode).json(resObj);
+  }
+
+  const { email, password } = matchedData(req);
 }
 
 export { loginUser };
