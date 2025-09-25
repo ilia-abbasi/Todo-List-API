c243fb410a0ff200d12d4902853754599432c267
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-25 18:01:22 +0330

    Added describe for Bob registering and creating todo item

--- src/__tests__/e2e/todos.spec.mjs
+++ src/__tests__/e2e/todos.spec.mjs
@@ -6,7 +6,8 @@ import { createApp } from "../../app/app.mjs";
 import mocks from "../../helpers/mocks.mjs";
 
 const app = createApp();
-let jwt = "";
+let jwtJohn;
+let jwtBob;
 let todoDataJohn;
 let todoDataBob;
 let UpdatedTodoDataJohn;
@@ -28,7 +29,7 @@ describe("John registers and creates a todo item", () => {
 
     expect(response.statusCode).toEqual(201);
 
-    jwt = response.body.data.token;
+    jwtJohn = response.body.data.token;
   });
 
   it("should check the JWT when creating a todo item", async () => {
@@ -38,7 +39,33 @@ describe("John registers and creates a todo item", () => {
   it("should successfully create a todo item", async () => {
     await request(app)
       .post("/todos")
-      .set("Authorization", `Bearer ${jwt}`)
+      .set("Authorization", `Bearer ${jwtJohn}`)
+      .send(mocks.todoData)
+      .expect(201);
+  });
+});
+
+describe("Bob registers and creates a todo item", () => {
+  it("should register successfully", async () => {
+    const response = await request(app).post("/register").send({
+      name: mocks.userDataBob.name,
+      email: mocks.userDataBob.email,
+      password: mocks.userDataBob.password,
+    });
+
+    expect(response.statusCode).toEqual(201);
+
+    jwtBob = response.body.data.token;
+  });
+
+  it("should check the JWT when creating a todo item", async () => {
+    await request(app).post("/todos").send(mocks.todoData).expect(401);
+  });
+
+  it("should successfully create a todo item", async () => {
+    await request(app)
+      .post("/todos")
+      .set("Authorization", `Bearer ${jwtBob}`)
       .send(mocks.todoData)
       .expect(201);
   });
