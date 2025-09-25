6edf0be13ea8f98a092ac07fbe171fc7d0cb386e
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-25 15:40:53 +0330

    Added 405 error tests for /register and /login

--- src/__tests__/e2e/errors.spec.mjs
+++ src/__tests__/e2e/errors.spec.mjs
@@ -5,14 +5,26 @@ const app = createApp();
 
 describe("general 404 errors", () => {
   it("should return 404 error on GET /non-existant/path", async () => {
-    const response = await request(app).get("/non-existant/path");
-
-    expect(response.statusCode).toEqual(404);
+    await request(app).get("/non-existant/path").expect(404);
   });
 
   it("should return 404 error on PUT /undefined/path", async () => {
-    const response = await request(app).put("/undefined/path");
+    await request(app).put("/undefined/path").expect(404);
+  });
+});
 
-    expect(response.statusCode).toEqual(404);
+describe("405 errors", () => {
+  it("should get 405 error on DELETE /register", async () => {
+    const response = await request(app).delete("/register");
+
+    expect(response.statusCode).toEqual(405);
+    expect(response.headers.allow).toEqual("POST");
+  });
+
+  it("should get 405 error on GET /login", async () => {
+    const response = await request(app).get("/login");
+
+    expect(response.statusCode).toEqual(405);
+    expect(response.headers.allow).toEqual("POST");
   });
 });
