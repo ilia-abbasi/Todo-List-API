c602b74c74c18c6eb23efcd54d85806cc716778d
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-09-25 18:14:13 +0330

    Added test for PUT /todos/todoId
    - Changed some other tests in todos.spec.mjs

--- src/__tests__/e2e/todos.spec.mjs
+++ src/__tests__/e2e/todos.spec.mjs
@@ -10,7 +10,10 @@ let jwtJohn;
 let jwtBob;
 let todoDataJohn;
 let todoDataBob;
-let UpdatedTodoDataJohn;
+let UpdatedTodoData = {
+  title: "[NEW] Clean room",
+  description: "[NEW] Use a broom to clean the bedroom",
+};
 
 db.initializePool({ test: true });
 
@@ -19,43 +22,24 @@ afterAll(async () => {
   await db.endPool();
 });
 
-describe("John registers and creates a todo item", () => {
+describe("John and Bob register and create a todo item", () => {
   it("should register successfully", async () => {
-    const response = await request(app).post("/register").send({
+    const responseJohn = await request(app).post("/register").send({
       name: mocks.userDataJohn.name,
       email: mocks.userDataJohn.email,
       password: mocks.userDataJohn.password,
     });
-
-    expect(response.statusCode).toEqual(201);
-
-    jwtJohn = response.body.data.token;
-  });
-
-  it("should check the JWT when creating a todo item", async () => {
-    await request(app).post("/todos").send(mocks.todoData).expect(401);
-  });
-
-  it("should successfully create a todo item", async () => {
-    await request(app)
-      .post("/todos")
-      .set("Authorization", `Bearer ${jwtJohn}`)
-      .send(mocks.todoData)
-      .expect(201);
-  });
-});
-
-describe("Bob registers and creates a todo item", () => {
-  it("should register successfully", async () => {
-    const response = await request(app).post("/register").send({
+    const responseBob = await request(app).post("/register").send({
       name: mocks.userDataBob.name,
       email: mocks.userDataBob.email,
       password: mocks.userDataBob.password,
     });
 
-    expect(response.statusCode).toEqual(201);
+    expect(responseJohn.statusCode).toEqual(201);
+    expect(responseBob.statusCode).toEqual(201);
 
-    jwtBob = response.body.data.token;
+    jwtJohn = responseJohn.body.data.token;
+    jwtBob = responseBob.body.data.token;
   });
 
   it("should check the JWT when creating a todo item", async () => {
@@ -63,10 +47,37 @@ describe("Bob registers and creates a todo item", () => {
   });
 
   it("should successfully create a todo item", async () => {
-    await request(app)
+    const responseJohn = await request(app)
+      .post("/todos")
+      .set("Authorization", `Bearer ${jwtJohn}`)
+      .send(mocks.todoData);
+    const responseBob = await request(app)
       .post("/todos")
       .set("Authorization", `Bearer ${jwtBob}`)
-      .send(mocks.todoData)
-      .expect(201);
+      .send(mocks.todoData);
+
+    expect(responseJohn.statusCode).toEqual(201);
+    expect(responseBob.statusCode).toEqual(201);
+
+    todoDataJohn = responseJohn.body.data;
+    todoDataBob = responseBob.body.data;
+  });
+});
+
+describe("John tries to update the todo items", () => {
+  it("should fail in updating Bob's todo item", async () => {
+    await request(app)
+      .put(`/todos/${todoDataBob.todo_id}`)
+      .set("Authorization", `Bearer ${jwtJohn}`)
+      .send(UpdatedTodoData)
+      .expect(403);
+  });
+
+  it("should succeed in updating his todo item", async () => {
+    await request(app)
+      .put(`/todos/${todoDataJohn.todo_id}`)
+      .set("Authorization", `Bearer ${jwtJohn}`)
+      .send(UpdatedTodoData)
+      .expect(200);
   });
 });
